a1ca06c17d1db00ada6191d6f1e8f61a140630fb
unknown <hoang0403ss@gmail.com>
2024-09-12 09:35:27 +0700

    noise image and image by ddpm

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ import argparse
 parser = argparse.ArgumentParser()
 args = parser.parse_args()
 args.batch_size = 5
-args.dataset_path = r""
+args.dataset_path = r"D:\Colorization\food_2"
 args.labels = 10
+args.image_size = 64
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 diffusion = Diffusion
 model = Unet()
@@ -17,7 +18,11 @@ model.eval()
 
 dataloader = get_data(args)
 test_batch = next(iter(dataloader))[0]
+t = torch.Tensor([50, 100, 150, 200, 300, 400, 500]).long()
+
+noise_image, _ = diffusion.noise_image(test_batch, t)
 
 images = diffusion.sample(model, args.batch_size, args.labels)
 
+plot_images(noise_image)
 plot_images(images)
